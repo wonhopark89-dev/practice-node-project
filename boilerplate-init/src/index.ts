49ec476eb3f3660ec7dc3e8c5f1bc393ed4d3c72
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-09-12 01:40:35 +0900

    Add mongoose and es6 compatibility

--- boilerplate-init/src/index.ts
+++ boilerplate-init/src/index.ts
@@ -1,6 +1,16 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+
+// const express = require('express');
 const app = express();
 const port = 4000;
+
+const MONGODB_USER_URL =
+  'mongodb+srv://wonhopark:dnjsghxptmxm1!@practice-node.b3nrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+// https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
+const dbConnection = mongoose.connect(MONGODB_USER_URL);
+dbConnection.then(() => console.log('MongoDB Connected...')).catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
