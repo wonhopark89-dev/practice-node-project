2c4cb2f9991468ad53addac198d028b77e3c8f88
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-09-12 02:08:56 +0900

    add save to db

--- boilerplate-init/src/index.ts
+++ boilerplate-init/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import {User} from './models/User';
+import bodyParser from 'body-parser';
 // const express = require('express');
 const app = express();
 const port = 4000;
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 const MONGODB_USER_URL =
   'mongodb+srv://wonhopark:dnjsghxptmxm1!@practice-node.b3nrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
@@ -14,6 +20,16 @@ dbConnection.then(() => console.log('MongoDB Connected...')).catch((err) => cons
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({success: false, err});
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
