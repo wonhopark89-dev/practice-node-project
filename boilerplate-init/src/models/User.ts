133731bcf342c8cb7903741e2966d50773aa7d52
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-09-12 16:45:04 +0900

    Add bcrypt with mongoose

--- boilerplate-init/src/models/User.ts
+++ boilerplate-init/src/models/User.ts
@@ -1,4 +1,7 @@
 import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10; // 10자리 salt
 
 // https://mongoosejs.com/docs/guide.html#definition
 const userSchema = new Schema({
@@ -26,6 +29,32 @@ const userSchema = new Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// save 를 수행전
+userSchema.pre('save', function (next) {
+  // userSchema
+  let user = this;
+
+  // password 를 수정할때만 작동되도록 설정
+  // name, email 수정할때 hash 동작하지 않도록
+  if (user.isModified('password')) {
+    // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        // Store hash in your password DB.
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 
 // https://mongoosejs.com/docs/guide.html#models
