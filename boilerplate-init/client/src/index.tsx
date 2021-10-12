02c36ddac9562e61bc37f6274c937ec080e43e36
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-13 01:55:30 +0900

    ᕙ(⇀‸↼‶)ᕗ Add redux with middleware

--- boilerplate-init/client/src/index.tsx
+++ boilerplate-init/client/src/index.tsx
@@ -4,14 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+// createStore 는 객체밖에 저장하지 못한다.
+// promise 와 function 을 저장하기 위해 middleware 로 추가
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(Reducer)}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
