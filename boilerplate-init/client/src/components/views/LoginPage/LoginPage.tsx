83d13705086a1d1df3bd2f71674ac07f3958017e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-14 01:30:24 +0900

    ᕙ(⇀‸↼‶)ᕗ Add login ui, form and input

--- boilerplate-init/client/src/components/views/LoginPage/LoginPage.tsx
+++ boilerplate-init/client/src/components/views/LoginPage/LoginPage.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onEmailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault(); // prevent refresh
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form style={{display: 'flex', flexDirection: 'column'}}>
+        <label>Email</label>
+        <input type={'email'} value={email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type={'current-password'} value={password} onChange={onPasswordHandler} />
+        <br />
+        <button type={'submit'} onSubmit={onSubmitHandler}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
