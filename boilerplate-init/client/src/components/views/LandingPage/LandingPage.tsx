eb63ec3e33fc23b8fe4dc90776fc00b986801ea0
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-17 02:09:07 +0900

    ᕙ(⇀‸↼‶)ᕗ Add logout user draft

--- boilerplate-init/client/src/components/views/LandingPage/LandingPage.tsx
+++ boilerplate-init/client/src/components/views/LandingPage/LandingPage.tsx
@@ -1,10 +1,21 @@
 import React, {useEffect} from 'react';
 import axios from 'axios';
+import {RouteComponentProps} from 'react-router-dom';
 
-const LandingPage = () => {
+const LandingPage = ({history}: RouteComponentProps) => {
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((response: any) => {
+      if (response.data.success) {
+        history.push('/login');
+      } else {
+        alert('로그아웃 실패');
+      }
+    });
+  };
 
   return (
     <div
@@ -17,6 +28,8 @@ const LandingPage = () => {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 };
